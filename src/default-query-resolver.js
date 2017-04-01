beb9352c72d95d4eac2d593a1b856192403f365f
Emanuel Jöbstl <emanuel.joebstl@gmail.com>
2017-04-01 23:30:04 +0200

    Added persistent retry, three times, to default query resolver.

--- src/default-query-resolver.js
+++ src/default-query-resolver.js
@@ -4,28 +4,40 @@ export default function createQueryResolver(endpoint, headers) {
   // Create query resolver function
   return async function(query, vars) {
     // For default, we're using raw HTTP and rely on the server for validation. 
-    const res = await popsicle.request({
-      method: 'POST',
-      url: endpoint,
-      headers: headers || { },
-      body: {
-        query: query,
-        variables: vars || { }
+
+    let retryCount = 3
+
+    while(true) {
+      const res = await popsicle.request({
+        method: 'POST',
+        url: endpoint,
+        headers: headers || { },
+        body: {
+          query: query,
+          variables: vars || { }
+        }
+      })
+
+      if(res.status != 200) {
+        if(retryCount <= 0) {
+          // If we have a HTTP error when querying, we retry. 
+          // This might be temporary due to high load. 
+          throw new Error(`Query error: Server respondend with ${res.status}, the response body is: ${res.body}, the generated query was: ${query}, the variables were: ${JSON.stringify(vars)}`)
+        } else {
+          retryCount--
+          continur
+        }
       }
-    })
+      
+      const rbody = JSON.parse(res.body)
 
-    if(res.status != 200) {
-      throw new Error(`Query error: Server respondend with ${res.status}, the response body is: ${res.body}, the generated query was: ${query}, the variables were: ${JSON.stringify(vars)}`)
-    }
-    
-    const rbody = JSON.parse(res.body)
-
-    if(rbody.error) {
-      throw new Error(`Query error: ${rbody.error.message}`)
-    } else if(rbody.errors) {
-      throw new Error(`Query errors: ${rbody.errors.map((x) => x.message).join('\n')}`)
-    } else {
-      return rbody.data
+      if(rbody.error) {
+        throw new Error(`Query error: ${rbody.error.message}`)
+      } else if(rbody.errors) {
+        throw new Error(`Query errors: ${rbody.errors.map((x) => x.message).join('\n')}`)
+      } else {
+        return rbody.data
+      }
     }
   }
 }
